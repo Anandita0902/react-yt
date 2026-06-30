303ec1605492798c8db22b0f8e38679a4efc7cdf
Anandita0902 <ananditababar@gmail.com>
2026-07-01 00:21:57 +0530

    gallery done

--- 17-gallery-project/src/App.jsx
+++ 17-gallery-project/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Card from "./components/Card";
 
 const App = () => {
   const [userData, setUserData] = useState([]);
@@ -8,34 +9,30 @@ const App = () => {
 
   const getData = async () => {
     const response = await axios.get(
-      "https://picsum.photos/v2/list?page=2&limit=30",
+      ` https://picsum.photos/v2/list?page=${index}&limit=10`,
     );
 
     setUserData(response.data);
   };
 
-  useEffect(function () {
-    getData();
-  }, []);
+  useEffect(
+    function () {
+      getData();
+    },
+    [index],
+  );
 
   let printUserData = (
-    <h3 className="text-gray-400 text-xs">No user available</h3>
+    <h3 className="text-gray-400 text-xs absolute top1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold">
+      Loading...
+    </h3>
   );
 
   if (userData.length > 0) {
     printUserData = userData.map(function (elem, idx) {
       return (
         <div key={idx}>
-          <a href={elem.url} target="_blank">
-            <div className="h-40 w-44 overflow-hidden bg-white rounded-xl">
-              <img
-                className="h-full w-full object-cover"
-                src={elem.download_url}
-                alt=""
-              ></img>
-            </div>
-            <h2>{elem.author}</h2>
-          </a>
+          <Card elem={elem} />
         </div>
       );
     });
@@ -43,23 +40,28 @@ const App = () => {
 
   return (
     <div className="bg-black overflow-auto h-screen p-4 text-white">
-      <div className="flex flex-wrap gap-4">{printUserData}</div>
+      <div className="flex h-[82%] flex-wrap gap-4 p-2">{printUserData}</div>
 
-      <div className="flex justify-center items-center p-4 gap-4 ">
+      <div className="flex justify-center items-center p-4 gap-6 ">
         <button
-          className="bg-amber-400 active:scale-95 mb-3 px-5 py-2 rounded text-white "
+          style={{ opacity: index == 1 ? 0.5 : 1 }}
+          className="bg-amber-400 text-sm cursor-pointer active:scale-95  text-black rounded px-4 py-2 font-semibold "
           onClick={() => {
-            setIndex(index - 1);
+            if (index > 1) {
+              setIndex(index - 1);
+              setUserData([]);
+            }
           }}
         >
           Prev
         </button>
+        <h4>Page {index}</h4>
         <button
-          className="bg-amber-400 active:scale-95 mb-3 px-5 py-2 rounded text-white "
+          className="bg-amber-400 text-sm cursor-pointer active:scale-95  text-black rounded px-4 py-2 font-semibold "
           onClick={() => {
-            if (index > 1) {
-              setIndex(index + 1);
-            }
+            setUserData([]);
+
+            setIndex(index + 1);
           }}
         >
           Next
